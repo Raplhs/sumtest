860f7652494d516f310bdfd19ab301445c2817e1
Jammaz <103277643+Raplhs@users.noreply.github.com>
2024-01-22 03:47:43 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const world = require(`./console/world`);
-const mess = require(`./messages/message`);
+const world = require(`./world`);
+const mess = require(`./message`);
 
 console.log(mess, mess, "cool");
